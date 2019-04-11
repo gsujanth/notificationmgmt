0efb6587e96d52e38b3d72e4d5029073ac562f6f
gsujanth <princesujanth@gmail.com>
2019-04-10 21:48:03 -0400

    test in NotificationmgmtApplicationTests

--- src/test/java/com/rabbitmq/notifications/notificationmgmt/NotificationmgmtApplicationTests.java
+++ src/test/java/com/rabbitmq/notifications/notificationmgmt/NotificationmgmtApplicationTests.java
@@ -1,16 +1,24 @@
 package com.rabbitmq.notifications.notificationmgmt;
 
+import com.rabbitmq.notifications.notificationmgmt.Sender.MessageSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NotificationmgmtApplicationTests {
 
+	@Autowired
+	MessageSender messageSender;
+
 	@Test
 	public void contextLoads() {
+		assertThat(messageSender).isNotNull();
 	}
 
 }
